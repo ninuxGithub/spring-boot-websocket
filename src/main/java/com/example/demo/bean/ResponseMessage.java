90cc2f0d12347c43886ac208bcb539c3c45e4e7b
ninuxGithub <1732768613@qq.com>
2017-10-16 17:24:29 +0800

    init

---
+++ src/main/java/com/example/demo/bean/ResponseMessage.java
@@ -0,0 +1,14 @@
+package com.example.demo.bean;
+
+public class ResponseMessage {
+	private String responseMessage;
+
+	public ResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+
+}
