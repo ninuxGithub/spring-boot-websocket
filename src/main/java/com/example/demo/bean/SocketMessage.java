c8efaefc7743dcfaf7ff2d4ad7f088fd1f121f28
ninuxGithub <1732768613@qq.com>
2017-10-16 17:36:25 +0800

    消息的推送

---
+++ src/main/java/com/example/demo/bean/SocketMessage.java
@@ -0,0 +1,24 @@
+package com.example.demo.bean;
+
+public class SocketMessage {
+	private String message;
+
+	private String date;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -9,13 +9,26 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+	// @Override
+	// public void registerStompEndpoints(StompEndpointRegistry
+	// stompEndpointRegistry) {
+	// stompEndpointRegistry.addEndpoint("/endpointSang").withSockJS();
+	// }
+	//
+	// @Override
+	// public void configureMessageBroker(MessageBrokerRegistry registry) {
+	// registry.enableSimpleBroker("/topic");
+	// }
+
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-		stompEndpointRegistry.addEndpoint("/endpointSang").withSockJS();
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/topic");
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/my-websocket").withSockJS();
 	}
+
 }
